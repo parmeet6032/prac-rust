68b482255c13da3006dbcb832532a908107a6892
Parmeet Singh <singh.pmeet752@gmail.com>
2024-03-12 04:23:58 +0530

    testing

--- basics/variables/src/main.rs
+++ basics/variables/src/main.rs
@@ -2,7 +2,7 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-  let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+  let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
 
   println!("Firing {} of my {} missiles...", ready, missiles);
   // missiles = missiles - ready;
